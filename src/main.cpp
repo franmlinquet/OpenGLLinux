f148c7055d8e82024e603035d3c21753a9ae85d1
Franz M. Linquet <franmlinquet@gmail.com>
2025-08-27 08:59:01 -0300

    vertex & fragment shaders sources added

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,20 @@
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
 
+const char *vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+
+const char *fragmentShaderSource = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(1.0, 0.0, 0.0, 1.0);\n"
+    "}\0";
+
 int main()
 {
     // Initialize GLFW
