b6d2c390299ccbad520fa38ac0b7cb2499637464
Franz M. Linquet <franmlinquet@gmail.com>
2025-09-03 08:16:02 -0300

    Working on Shader.h

--- src/Shader.h
+++ src/Shader.h
@@ -11,7 +11,16 @@ class Shader {
     public:
         Shader(const std::string& vertexPath, const std::string& fragmentPath) 
         {
-            
+            std::string vertexCode;
+            std::string fragmentCode;
+            std::ifstream vShaderFile;
+            std::ifstream fShaderFile;
+
+            vShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+            fShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+            try {
+                
+            }
         }
         void use();
     
