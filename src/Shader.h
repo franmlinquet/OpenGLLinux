5cbf66efb0263b392eabde24e6bb1d708bc494de
Franz M. Linquet <franmlinquet@gmail.com>
2025-09-03 08:27:58 -0300

    Shader done

--- src/Shader.h
+++ src/Shader.h
@@ -9,6 +9,7 @@
 
 class Shader {
     public:
+    GLuint ID;
         Shader(const std::string& vertexPath, const std::string& fragmentPath) 
         {
             std::string vertexCode;
@@ -34,7 +35,37 @@ class Shader {
             }
             const char* vShaderCode = vertexCode.c_str();
             const char* fShaderCode = fragmentCode.c_str();
-            
+            GLuint vertex, fragment;
+            int success;
+            char infoLog[512];
+
+            vertex = glCreateShader(GL_VERTEX_SHADER);
+            glShaderSource(vertex, 1, &vShaderCode, NULL);
+            glCompileShader(vertex);
+
+            glGetShaderiv(vertex, GL_COMPILE_STATUS, &success);
+            if (!success) {
+                glGetShaderInfoLog(vertex, 512, NULL, infoLog);
+                std::cerr << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+            }
+
+            fragment = glCreateShader(GL_FRAGMENT_SHADER);
+            glShaderSource(fragment, 1, &fShaderCode, NULL);
+            glCompileShader(fragment);
+
+            glGetShaderiv(fragment, GL_COMPILE_STATUS, &success);
+            if (!success) {
+                glGetShaderInfoLog(fragment, 512, NULL, infoLog);
+                std::cerr << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+            }
+
+             ID = glCreateProgram();
+            glAttachShader(ID, vertex);
+            glAttachShader(ID, fragment);
+            glLinkProgram(ID);
+
+            glDeleteShader(vertex);
+            glDeleteShader(fragment);
         }
         void use();
     
