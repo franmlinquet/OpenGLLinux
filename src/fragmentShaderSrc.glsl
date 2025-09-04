a949d17f6e1a41d44b8f9b0fd3163aa09461b283
Franz M. Linquet <franmlinquet@gmail.com>
2025-09-04 13:20:23 -0300

    textures created

--- src/fragmentShaderSrc.glsl
+++ src/fragmentShaderSrc.glsl
@@ -6,5 +6,5 @@ uniform sampler2D texture1;
 uniform sampler2D texture2;
 void main()
 {
-   FragColor = mix(texture(TexCoord, texture1), texture(TexCoord, texture2));
+   FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0,2);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -76,9 +76,51 @@ int main()
     glBindVertexArray(0);
 
 
-    GLuint texture;
-    glGenTextures(1, &texture);
-    glBindTexture(GL_TEXTURE_2D, texture);
+    GLuint texture1, texture2;
+    int width, height, nrChannels;
+    {
+        glGenTextures(1, &texture1);
+        glBindTexture(GL_TEXTURE_2D, texture1);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    
+        unsigned char* data = stbi_load("assets/textures/container.jpg", &width, &height, &nrChannels, 0);
+    
+        if(data) 
+        {
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+            glGenerateMipmap(GL_TEXTURE_2D);
+        }
+        else 
+        {
+            std::cerr << "Failed to load texture" << std::endl;
+        }
+        stbi_image_free(data);
+    }
+    
+    {
+        glGenTextures(1, &texture2);
+        glBindTexture(GL_TEXTURE_2D, texture2);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    
+        unsigned char* data = stbi_load("assets/textures/awesomeface.png", &width, &height, &nrChannels, 0);
+    
+        if(data) 
+        {
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+            glGenerateMipmap(GL_TEXTURE_2D);
+        }
+        else 
+        {
+            std::cerr << "Failed to load texture" << std::endl;
+        }
+        stbi_image_free(data);
+    }
 
     // Main loop
     while (!glfwWindowShouldClose(window))
