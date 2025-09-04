23effc859d0b568224dae5fc937fccb91f830336
Franz M. Linquet <franmlinquet@gmail.com>
2025-09-04 13:01:55 -0300

    STB IMAGE IMPLEMENTED

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#include <iostream>
 #include "Shader.h"
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#include <iostream>
+
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
