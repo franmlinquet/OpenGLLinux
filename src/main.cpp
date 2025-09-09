abbafcd5aae48cd2c74cfea6b16ed69c04144b80
Franz M. Linquet <franmlinquet@gmail.com>
2025-09-09 09:37:57 -0300

    Translate & Rotation

--- src/main.cpp
+++ src/main.cpp
@@ -168,8 +168,9 @@ int main()
         glm::mat4 trans = glm::mat4(1.0f);
         
         /* Rotation */
-        trans = glm::rotate(trans, glm::radians(90.0f), glm::vec3(0.0, 0.0, 1.0));
-        trans = glm::scale(trans, glm::vec3(0.5f, 0.5f, 0.5f));
+        trans = glm::translate(trans, glm::vec3(0.5f, -0.5f, 0.0f));
+        trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0.0, 0.0, 1.0));
+        //trans = glm::scale(trans, glm::vec3(0.5f, 0.5f, 0.5f));
         
         unsigned int transformLoc = glGetUniformLocation(shader.ID, "transform");
         glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
