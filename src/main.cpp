099c9ef39d11752e221b445fda6c2edcd6d0ec5f
Franz M. Linquet <franmlinquet@gmail.com>
2025-09-11 10:02:31 -0300

    Uniform manipulated from render loop

--- src/main.cpp
+++ src/main.cpp
@@ -181,10 +181,19 @@ int main()
         model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
         // View
         glm::mat4 view = glm::mat4(1.0f);
-        view = glm::translate(model, glm::vec3(0.0f, 0.0f, -3.0f));
+        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
         // Projection
         glm::mat4 projection;
         projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+
+        int modelLoc = glGetUniformLocation(shader.ID, "model");
+        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+
+        int viewLoc = glGetUniformLocation(shader.ID, "view");
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+
+        int projectionLoc = glGetUniformLocation(shader.ID, "projection");
+        glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
 
         shader.use();
         glBindVertexArray(VAO);
