d17dd79a4a17fa58f0026b5c710232a24f1174b3
Franz M. Linquet <franmlinquet@gmail.com>
2025-09-12 08:58:32 -0300

    Cleaner code

--- src/main.cpp
+++ src/main.cpp
@@ -188,6 +188,9 @@ int main()
     shader.setInt("texture1", 0);
     shader.setInt("texture2", 1);
 
+    glm::mat4 projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+    shader.setMat4("projection", projection);
+
     // Main loop
     while (!glfwWindowShouldClose(window))
     {
@@ -195,22 +198,18 @@ int main()
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, texture1);
         glActiveTexture(GL_TEXTURE1);
         glBindTexture(GL_TEXTURE_2D, texture2);
-
         
         shader.use();
-        //glm::mat4 model = glm::mat4(1.0f);
-        //model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(0.5f, 1.0f, 0.0f));
-        glm::mat4 view       = glm::mat4(1.0f);
-        glm::mat4 projection = glm::mat4(1.0f);
-        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
-        projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
 
-        shader.setMat4("projection", projection);
+        glm::mat4 view = glm::mat4(1.0f);
+        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+        
+
+        
         shader.setMat4("view", view);
         
         glBindVertexArray(VAO);
