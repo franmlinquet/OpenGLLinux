7f7db9bd088b4abefc715a8c22abe96beb12dde2
Franz M. Linquet <franmlinquet@gmail.com>
2025-09-12 09:05:52 -0300

    Spinning Camera!

--- src/main.cpp
+++ src/main.cpp
@@ -206,10 +206,12 @@ int main()
         shader.use();
 
         glm::mat4 view = glm::mat4(1.0f);
-        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
-        
-
-        
+        float radius = 10.0f;
+        float camX = static_cast<float>(sin(glfwGetTime()) * radius);
+        float camZ = static_cast<float>(cos(glfwGetTime()) * radius);
+        view =  glm::lookAt(glm::vec3(camX, 0.0f, camZ), 
+                            glm::vec3(0.0f, 0.0f, 0.0f), 
+                            glm::vec3(0.0f, 1.0f, 0.0f));       
         shader.setMat4("view", view);
         
         glBindVertexArray(VAO);
