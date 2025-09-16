1c41f74fca1cf624299b1d1a859b2fd40d016c17
Franz M. Linquet <franmlinquet@gmail.com>
2025-09-16 10:45:54 -0300

    Calculated the direction vector and given to camera

--- src/main.cpp
+++ src/main.cpp
@@ -301,7 +301,14 @@ void mouse_callback(GLFWwindow* window, double xposIn, double yposIn)
     if (pitch > 89.0)
         pitch = 89.0;
     if (pitch < -89.0)
-        pitch = -89.0,
+        pitch = -89.0;
+
+    glm::vec3 front;
+    front.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+    front.y = sin(glm::radians(pitch));
+    front.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+    cameraFront = glm::normalize(front);
+
 }
 
 void processInput(GLFWwindow* window)
