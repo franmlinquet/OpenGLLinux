097eea5981bbcb347c796f535426016851a60c28
Franz M. Linquet <franmlinquet@gmail.com>
2025-09-17 09:40:37 -0300

    Camera Header with a Class created

---
+++ src/Camera.h
@@ -0,0 +1,21 @@
+#ifndef CAMERA_H
+#define CAMERA_H
+
+// ENUM
+
+// DEFAULT VALUES
+
+class Camera
+{
+    public:
+    // Attributes
+    // Euler Angles
+    // Options
+
+    // Constructors
+
+    // View Matrix
+
+    // Input Process
+};
+#endif
