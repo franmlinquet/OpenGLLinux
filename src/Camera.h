6f8dbd02d43978e4e844369d5882eff6835844fe
Franz M. Linquet <franmlinquet@gmail.com>
2025-09-17 10:05:59 -0300

    Constructors implemented (takes Vectors and/or Scalar values)

--- src/Camera.h
+++ src/Camera.h
@@ -25,10 +25,36 @@ class Camera
 {
     public:
     // Attributes
+    glm::vec3 Position;
+    glm::vec3 Front;
+    glm::vec3 Up;
+    glm::vec3 Right
+    glm::vec3 WorldUp;
     // Euler Angles
+    float Yaw;
+    float Pitch;
     // Options
+    float MovementSpeed;
+    float MouseSensitivity;
+    float Zoom;
 
     // Constructors
+    Camera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f), float yaw = YAW, float pitch = PITCH) : Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVITY), Zoom(ZOOM)
+    {
+        Position = position;
+        WorldUp = up;
+        Yaw = yaw;
+        Pitch = pitch;
+        updateCameraVector();
+    }
+    Camera(float posX, float posY, float posZ, float upX, float upY, float upZ, float yaw, float pitch) : Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVITY), Zoom(ZOOM)
+    {
+        Position = glm::vec3(posX, posY, posZ);
+        WorldUp = glm::vec3(upX, upY, upZ);
+        Yaw = yaw;
+        Pitch = pitch;
+        updateCameraVector();
+    }
 
     // View Matrix
 
