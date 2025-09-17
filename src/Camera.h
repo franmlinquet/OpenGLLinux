c7a2e45e9dc242b16db0a02f543f2b2077b5b4c9
Franz M. Linquet <franmlinquet@gmail.com>
2025-09-17 09:53:41 -0300

    Enum & Default Values added to Camera Class

--- src/Camera.h
+++ src/Camera.h
@@ -1,9 +1,25 @@
 #ifndef CAMERA_H
 #define CAMERA_H
 
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
 // ENUM
+enum Camera_Movement
+{
+    FORWARD,
+    BACLWARD,
+    LEFT, 
+    RIGHT
+};
 
 // DEFAULT VALUES
+const float YAW         = -90.0f;
+const float PITCH       = 0.0f;
+const float SPEED       = 2.5f;
+const float SENSITIVITY = 0.1f;
+const float ZOOM        = 45.0f;
 
 class Camera
 {
