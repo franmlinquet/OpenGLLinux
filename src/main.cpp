ee383a9ef55f7bed35d36df2411645597571175f
Franz M. Linquet <franmlinquet@gmail.com>
2025-10-07 13:20:05 -0300

    Cube & LightCube implemented

--- src/main.cpp
+++ src/main.cpp
@@ -37,6 +37,8 @@ bool firstMouse = true;
 float deltaTime = 0.0f;
 float lastFrame = 0.0f;
 
+glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
+
 int main()
 {
     // Initialize GLFW
@@ -78,7 +80,8 @@ int main()
     
     glEnable(GL_DEPTH_TEST);
     //Shader shader("../src/vertexShaderSrc.glsl", "../src/fragmentShaderSrc.glsl");
-    Shader lightingShader("../src/shaders/colors.vs", "../src/shaders/color.fs")
+    Shader lightingShader("../src/shaders/colors.vs", "../src/shaders/colors.fs");
+    Shader lightCubeShader("../src/shaders/lightCube.vs", "../src/shaders/lightCube.fs");
 
     float vertices[] = {
         -0.5f, -0.5f, -0.5f, 
@@ -151,35 +154,35 @@ int main()
         lastFrame = currentFrame;
         processInput(window);
         // Render here
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        glActiveTexture(GL_TEXTURE0);
-        glBindTexture(GL_TEXTURE_2D, texture1);
-        glActiveTexture(GL_TEXTURE1);
-        glBindTexture(GL_TEXTURE_2D, texture2);
-        
-        shader.use();
+        lightingShader.use();
+        lightingShader.setVec3("objectColor", 1.0f, 0.5f, 0.31f);
+        lightingShader.setVec3("lightColor", 1.0f, 1.0f, 1.0f);
 
-        glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), 800.0f / 600.0f, 0.1f, 100.0f);
-        shader.setMat4("projection", projection);
+        glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+        glm::mat4 view(camera.GetViewMatrix());
+        lightingShader.setMat4("projection", projection);
+        lightingShader.setMat4("view", view);
 
-        glm::mat4 view = camera.GetViewMatrix();
-        shader.setMat4("view", view);
-        
-        glBindVertexArray(VAO);
-        for (unsigned int i = 0; i < 10; i++)
-        {
-            glm::mat4 model = glm::mat4(1.0f);
-            model = glm::translate(model, cubePositions[i]);
-            float angle = 25.0f * i;
-            model = glm::rotate(model, (float)glfwGetTime() * glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
-            shader.setMat4("model", model);
+        glm::mat4 model =  glm::mat4(1.0f);
+        lightingShader.setMat4("model", model);
 
-            glDrawArrays(GL_TRIANGLES, 0, 36);
-        }
-        
-        
+        glBindVertexArray(cubeVAO);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+
+        lightCubeShader.use();
+        lightCubeShader.setMat4("projection", projection);
+        lightCubeShader.setMat4("view", view);
+        model = glm::mat4(1.0f);
+        model = translate(model, lightPos);
+        model = scale(model, glm::vec3(0.2f));
+        lightCubeShader.setMat4("model", model);
+
+        glBindVertexArray(lightCubeVAO);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+
 
         // Swap front and back buffers
         glfwSwapBuffers(window);
@@ -188,7 +191,8 @@ int main()
         glfwPollEvents();
     }
 
-    glDeleteVertexArrays(1, &VAO);
+    glDeleteVertexArrays(1, &cubeVAO);
+    glDeleteVertexArrays(1, &lightCubeVAO);
     glDeleteBuffers(1, &VBO);
     //glDeleteBuffers(1, &EBO);
 
