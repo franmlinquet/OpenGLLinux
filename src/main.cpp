627f79b72a78730f15da3ea0c0846282ad7fb0eb
Franz M. Linquet <franmlinquet@gmail.com>
2025-10-21 08:20:31 -0300

    update

--- src/main.cpp
+++ src/main.cpp
@@ -141,7 +141,7 @@ int main()
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float))); 
     glEnableVertexAttribArray(1);
 
     glGenVertexArrays(1, &lightCubeVAO);
